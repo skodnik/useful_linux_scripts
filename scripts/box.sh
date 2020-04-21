08b7a425fae2168e838e8ef9284767b88ec22450
skodnik <g.skodnik@gmail.com>
2020-04-21 15:57:54 +0300

    Update box.sh

--- scripts/box.sh
+++ scripts/box.sh
@@ -2,6 +2,8 @@
 
 # Upload file to box.com
 
+# Example: $ bash box.sh V7Fe9siRTHEs3TmjQOa9F2QoxhmNHjBq box.sh ./box.sh
+
 # Create Developer Token
 # https://app.box.com/developers/console/app/ -> app -> configuration -> create token
 
