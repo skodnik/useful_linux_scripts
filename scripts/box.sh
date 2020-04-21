77d87b370315ed0952973842ee4a3f0b207f856e
skodnik <g.skodnik@gmail.com>
2020-04-21 15:56:49 +0300

    Create box.sh

---
+++ scripts/box.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Upload file to box.com
+
+# Create Developer Token
+# https://app.box.com/developers/console/app/ -> app -> configuration -> create token
+
+# variables
+token="$1"
+filename="$2"
+filepath="$3"
+
+
+# https://developer.box.com/reference/post-files-content/
+curl -X POST https://upload.box.com/api/2.0/files/content \
+     -H "Authorization: Bearer $token" \
+     -H "Content-Type: multipart/form-data" \
+     -F attributes="{\"name\":\"$filename\", \"parent\":{\"id\":\"0\"}}" \
+     -F file=@$filepath
